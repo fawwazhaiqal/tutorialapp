9614e2de9ae1632ce3183e3c02d7ce028ccfda45
fawwazhaiqal <fawwazfuaad21@gmail.com>
2023-12-11 16:33:58 +0800

    episode 8

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<title>myblog </title>
+<link rel="stylesheet"href="/app.css">
+
+<body>
+        <article>
+                 <?= $post;?>
+        </article>     
+
+          
+        <a href ="/">Go back</a>
+        
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+
+<title>myblog </title>
+<link rel="stylesheet"href="/app.css">
+
+<body>
+        <article>
+                 <h1><a href="/post">My First Posts</a></h1>
+
+        <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores eligendi est corrupti voluptas nulla officiis enim rem reprehenderit quae, consectetur ducimus aliquam amet fugit magni placeat ratione, doloremque quibusdam eius.
+        </p>
+        </article>     
+
+        <article>
+                <h1><a href="/post">My Second Posts</a></h1>
+
+       <p>
+               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores eligendi est corrupti voluptas nulla officiis enim rem reprehenderit quae, consectetur ducimus aliquam amet fugit magni placeat ratione, doloremque quibusdam eius.
+       </p>
+       </article>     
+
+       <article>
+        <h1><a href="/post">My Third Posts</a></h1>
+
+        <p>
+         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores eligendi est corrupti voluptas nulla officiis enim rem reprehenderit quae, consectetur ducimus aliquam amet fugit magni placeat ratione, doloremque quibusdam eius.
+        </p>
+        </article>     
+        
+        
+</body>
